3487f84f0232690d4c41d9bf917b0c7a71aa27c7
Paul Tomchik <pauljtomchik@gmail.com>
2019-04-08 17:36:23 -0400

    Git Repo state util module.

---
+++ src/utils/GitRepoInfo.js
@@ -0,0 +1,11 @@
+const { execSync } = require('child_process');
+const { join } = require('path');
+
+const PROJECT_ROOT = join(__dirname, '../../');
+const opts = { cwd: PROJECT_ROOT, encoding: 'utf8' };
+
+const hash = execSync('git rev-parse HEAD', opts);
+
+const diff = execSync('git diff', opts);
+
+module.exports = { hash, diff };
